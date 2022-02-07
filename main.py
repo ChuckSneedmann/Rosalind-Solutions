26752ea6010b88840f574aafe0bfc6e02cfc4ee0
ChuckSneedmann <99034248+ChuckSneedmann@users.noreply.github.com>
2022-02-07 17:22:17 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+nucleotides = {'A':0,'C':0,'G':0,'T':0}
+
+with open("sampledata.txt", "r") as rawdata:
+    data = rawdata.read()
+    for i in data:
+        nucleotides[i] += 1
+
+print(nucleotides['A'],nucleotides['C'],nucleotides['G'],nucleotides['T'])
